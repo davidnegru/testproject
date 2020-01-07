0e25ded109d04b6e94e07ae116db9883d99b5dab
Adrian <Adrian@AdrianHP>
2020-01-07 21:37:36 +0200

    change 1 added class

---
+++ testProject/testClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * testClass.cpp
+ *
+ *  Created on: Jan 7, 2020
+ *      Author: Adrian
+ */
+
+#include "testClass.h"
+
+testClass::testClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+testClass::~testClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ testProject/testClass.h
@@ -0,0 +1,17 @@
+/*
+ * testClass.h
+ *
+ *  Created on: Jan 7, 2020
+ *      Author: Adrian
+ */
+
+#ifndef TESTCLASS_H_
+#define TESTCLASS_H_
+
+class testClass {
+public:
+	testClass();
+	virtual ~testClass();
+};
+
+#endif /* TESTCLASS_H_ */
